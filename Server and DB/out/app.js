13ec3faeed862e13a85cee0c0f830863fa0067d6
Hasan Berkan Eroğlu <72980770+HBE18@users.noreply.github.com>
2021-12-11 00:10:53 +0300

    Web Dev Basics
    Basic Localhost defined and server can be on with a basic get response.
    
    Co-Authored-By: Batuhan Kutlu <52199113+batuhankutlu@users.noreply.github.com>
    Co-Authored-By: HabipSanli <78694716+HabipSanli@users.noreply.github.com>

---
+++ Server and DB/out/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const constants_1 = require("./constants");
+const app = express_1.default();
+const config = require("../config.json");
+const port = 3000;
+app.use(express_1.default.json());
+const returner = "\t\t\t\tHello Natural Language Processers!";
+const msg = `App is running at http://localhost:${port}`;
+app.get("/", async (req, res) => {
+    res.status(constants_1.HttpCode.Success).send(returner);
+});
+console.log(msg);
+app.listen(port);
+//# sourceMappingURL=app.js.map
---
+++ Server and DB/src/constants.ts
@@ -0,0 +1,8 @@
+export enum HttpCode {
+    Success = 200,
+    Created = 201,
+    NoContent = 204,
+    BadRequest = 400,
+    Forbidden = 403,
+    Timeout = 504
+}
